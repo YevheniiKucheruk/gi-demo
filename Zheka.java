b0884961a914770d5196ec0b2348f871fb5279f7
Eugene <zhenya.kucheruk515@gmail.com>
2024-09-16 14:44:20 +0300

    The new branch commit

--- Zheka.java
+++ Zheka.java
@@ -1,5 +1,9 @@
+
 class Zheka{
   public static void main(String[] args){
     System.out.println("Hello world!");
+  }
+  public static double TheHypotenuse(double a, double b){
+    double Hypotenuse = 0;
   }
 }
